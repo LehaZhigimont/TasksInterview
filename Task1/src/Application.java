0a686acfd9a52e00cee957a0402bc84be4638f12
Admin <foto-minsk@ya.ru>
2019-04-12 08:42:55 +0300

    Added array numbers to the solution

--- Task1/src/Application.java
+++ Task1/src/Application.java
@@ -6,10 +6,9 @@ package com.zhigimont.by;
  */
 
 public class Application {
-    private static int[]curentArray = new int[]{3,3,3,2};
-
     public static void main(String[] args) {
+        Numbers numbers = new Numbers();
         Solutions solutions = new Solutions();
-        solutions.canBeEqualTo24(curentArray);
+        solutions.canBeEqualTo24(numbers.getNumbers());
     }
 }
